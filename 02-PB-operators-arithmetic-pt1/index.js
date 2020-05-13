aadc711cbd4ee3cb7bb978b5e0c9922bf9d69659
ungoot <ungoot@hotmail.com>
2020-05-13 11:30:55 +0200

    folders-renamed

---
+++ 02-PB-operators-arithmetic-pt1/index.js
@@ -0,0 +1,47 @@
+//0
+let x = 10;
+let y = 5;
+
+//1
+console.log(x+y);
+
+//2a
+console.log(y-x);
+
+//2b
+console.log(x-y);
+
+//3
+console.log(x*y);
+
+//4
+console.log(x/y);
+
+//5
+let z = 10;
+let resultOne = (x*y)/z;
+console.log(resultOne);
+
+//6
+let a = 15;
+let b = 9;
+console.log(a/b);
+
+//7
+let c = 20;
+let resultTwo = (a+b)*c;
+console.log(resultTwo);
+
+//8
+a = a++;
+
+//9
+b = b--;
+
+//10
+let d = b-a;
+console.log(d+c);
+
+//11
+
+console.log(resultOne%resultTwo);
